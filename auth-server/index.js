c72b355b0ca68deb29e187cb8b2425962bf229aa
Alice <alicemilea04@gmail.com>
2025-12-21 23:33:55 -0500

    fixed name being uppercase

--- auth-server/index.js
+++ auth-server/index.js
@@ -1,9 +1,7 @@
-
 import express from "express"; // needed for web server
 import cors from "cors"; // connects frontend to backend
 import dotenv from "dotenv"; // needed to get CLIENT_ID
 import { OAuth2Client } from "google-auth-library"; // needed to use google library
-
 
 // loads environment variables
 dotenv.config();
@@ -19,56 +17,54 @@ app.use(express.json());
 // creates instance of Google login
 const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
 
-
 // see if its an authorized user, and respond to frontend as such
 app.post("/api/auth/google", async (req, res) => {
-    try {
-        const token = req.body.token;
-        console.log("\n token = " + token);
-        // see if this is a valid Google Token
-        const ticket = await client.verifyIdToken({
-            idToken: token,
-            audience: process.env.GOOGLE_CLIENT_ID
-        });
+  try {
+    const token = req.body.token;
+    console.log("\n token = " + token);
+    // see if this is a valid Google Token
+    const ticket = await client.verifyIdToken({
+      idToken: token,
+      audience: process.env.GOOGLE_CLIENT_ID,
+    });
 
-        // info about user logging in
-        const payload = ticket.getPayload();
+    // info about user logging in
+    const payload = ticket.getPayload();
 
-        // get the user's email
-        const email = payload.email.toLowerCase();
+    // get the user's email
+    const email = payload.email.toLowerCase();
 
-        const validEmails = [ "rossitermackenzie@gmail.com", "Alicemilea04@gmail.com",
-                              "jmuaoesecretary@gmail.com" ];
+    const validEmails = [
+      "rossitermackenzie@gmail.com",
+      "alicemilea04@gmail.com",
+      "jmuaoesecretary@gmail.com",
+    ];
 
-        //todo: go back and coordinate this with alice's code
-        // if email is not an authorized email
-        if (!(validEmails.includes(email))) {
-            
-            // tell browser it’s forbidden
-            res.status(403);
-            
-            // send JSON response
-            res.json({authorized:false}) ;
-            
-            console.log("login not authorized");
-            return;
-        }
+    //todo: go back and coordinate this with alice's code
+    // if email is not an authorized email
+    if (!validEmails.includes(email)) {
+      // tell browser it’s forbidden
+      res.status(403);
 
-        // email is an authorized email
-        console.log("login authorized");
-        res.json({ authorized: true });
+      // send JSON response
+      res.json({ authorized: false });
 
+      console.log("login not authorized");
+      return;
     }
 
-    catch (err) {
-        console.error(err);
+    // email is an authorized email
+    console.log("login authorized");
+    res.json({ authorized: true });
+  } catch (err) {
+    console.error(err);
 
-        // tell browser it’s unauthorizes
-        res.status(401)
+    // tell browser it’s unauthorizes
+    res.status(401);
 
-        // send JSON response
-        res.json({ authorized: false });
-    }
+    // send JSON response
+    res.json({ authorized: false });
+  }
 });
 
 // start backend
