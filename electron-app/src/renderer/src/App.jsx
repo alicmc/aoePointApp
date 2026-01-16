5aa7becc06695347bf71a9e46fbd181eee8fcddf
Alice <alicemilea04@gmail.com>
2026-01-16 17:11:12 -0500

    converted to electron

---
+++ electron-app/src/renderer/src/App.jsx
@@ -0,0 +1,20 @@
+import "./App.css";
+import { HashRouter, Routes, Route } from "react-router-dom";
+import Login from "./components/Login";
+import Home from "./components/Home";
+
+function App() {
+  return (
+    <>
+      <HashRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+
+          <Route path="/Home" element={<Home />} />
+        </Routes>
+      </HashRouter>
+    </>
+  );
+}
+
+export default App;
